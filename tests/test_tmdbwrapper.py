1b50002499772b370df30e3e89dd679b5a157e5b
Stephan Klinger <staeff@arcor.de>
2018-06-04 18:03:38 +0200

    Use vcr to minimize API usage in tests

--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -1,5 +1,6 @@
 from pytest import fixture
 from tmdbwrapper import TV
+import vcr
 
 @fixture
 def tv_keys():
@@ -12,6 +13,7 @@ def tv_keys():
             'overview', 'popularity', 'backdrop_path',
             'first_air_date', 'vote_count', 'vote_average']
 
+@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')
 def test_tv_info(tv_keys):
     """Tests an API call to get a TV show's info"""
 
