75b2bf77adb75c99d3f0344a19f20ec6402f82c1
Stephan Klinger <staeff@arcor.de>
2018-06-04 17:42:11 +0200

    Add check for expected keys in result to test

--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -1,6 +1,18 @@
+from pytest import fixture
 from tmdbwrapper import TV
 
-def test_tv_info():
+@fixture
+def tv_keys():
+    """Return a static list of response keys
+    
+       This fixture is responsible to return only
+       the data for testing
+    """
+    return ['id', 'origin_country', 'poster_path', 'name',
+            'overview', 'popularity', 'backdrop_path',
+            'first_air_date', 'vote_count', 'vote_average']
+
+def test_tv_info(tv_keys):
     """Tests an API call to get a TV show's info"""
 
     tv_instance = TV(1396)
@@ -8,3 +20,4 @@ def test_tv_info():
 
     assert isinstance(response, dict)
     assert response['id'] == 1396, "The ID should be in the response"
+    assert set(tv_keys).issubset(response.keys()), "All keys should be in response"
