e3e9cdd6d32ab27799588fd8724102a34e11e01c
Stephan Klinger <staeff@arcor.de>
2018-06-04 18:31:25 +0200

    Remove the api key from stored results

--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -13,7 +13,7 @@ def tv_keys():
             'overview', 'popularity', 'backdrop_path',
             'first_air_date', 'vote_count', 'vote_average']
 
-@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')
+@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml', filter_query_parameters=['api_key'])
 def test_tv_info(tv_keys):
     """Tests an API call to get a TV show's info"""
 
@@ -24,7 +24,7 @@ def test_tv_info(tv_keys):
     assert response['id'] == 1396, "The ID should be in the response"
     assert set(tv_keys).issubset(response.keys()), "All keys should be in response"
 
-@vcr.use_cassette('tests/vcr_cassettes/tv-popular.yml')
+@vcr.use_cassette('tests/vcr_cassettes/tv-popular.yml', filter_query_parameters=['api_key'])
 def test_tv_popular(tv_keys):
     """Test an API call to get popular TV shows"""
 
